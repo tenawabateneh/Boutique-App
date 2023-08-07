2718e3689695bfeb669fc19f5fbeeb161051d0ea
Tenaw <tenawabateneh06@gmail.com>
2023-08-08 02:28:47 +0300

    18th commit personaly I have added the sign-in with google popup and redirect to the home page, by modifing the navigation and app component routing. after commit this version, cancle back the code to the original without a proper routing stage.

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />} >
-        <Route index element={<Home />} />
+        <Route index element={<Authentication />} />
+        <Route path='auth/*' element={<Authentication />} />
         <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
+        <Route path='home/*' element={<Home />} />
         <Route path='/checkout' element={<CheckOut />} />
       </Route>
     </Routes>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,5 @@
-import { Fragment, useContext } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Fragment, useContext, useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDroDown from "../../components/cart-dropdown/cart-dropdown.component";
@@ -21,6 +21,23 @@ const Navigation = () => {
   const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
 
+  const navigate = useNavigate();
+
+  // Redirect to the homepage
+  // useEffect(() => {
+  //   navigate("/auth");
+  // }, [LoggedIn, loggedOut]);
+
+  let LoggedIn = false;
+
+  useEffect(() => {
+    if (LoggedIn && currentUser) {
+      navigate("/home");
+    } else if (!LoggedIn) {
+      navigate("/auth");
+    }
+  }, [LoggedIn, currentUser]);
+
   return (
     <Fragment>
       <STC_NavigationContainer>
@@ -30,11 +47,15 @@ const Navigation = () => {
         <STC_NavLinks>
           <STC_NavLink to="/shop">SHOP</STC_NavLink>
           {currentUser ? (
-            <STC_NavLink as="span" onClick={signOutUser}>
+            <STC_NavLink to="/auth" as="span" onClick={signOutUser}>
               SIGN-OUT
+              {(LoggedIn = true)}
             </STC_NavLink>
           ) : (
-            <STC_NavLink to="/auth">SIGN-IN</STC_NavLink>
+            <STC_NavLink to="/auth">
+              SIGN-IN
+              {/* {setLoggedIn(false)} */}
+            </STC_NavLink>
           )}
           <CartIcon />
         </STC_NavLinks>
